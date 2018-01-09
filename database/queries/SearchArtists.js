19168a7fb30b7d7927bf561f10820ce3b5a2ba0a
Andrei Popovici <me@andreipopovici.co.uk>
2018-01-09 11:36:31 +0000

    Artist Project p1

---
+++ database/queries/SearchArtists.js
@@ -0,0 +1,51 @@
+const Artist = require('../models/artist');
+const mongoose = require('mongoose');
+
+/**
+ * Searches through the Artist collection
+ * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {string} sortProperty The property to sort the results by
+ * @param {integer} offset How many records to skip in the result set
+ * @param {integer} limit How many records to return in the result set
+ * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ */
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    const queryCriteria = buildQuery(criteria)
+    const query = Artist.find(queryCriteria)
+        .sort({ [sortProperty]: 1 })
+        .skip(offset)
+        .limit(limit);
+
+    return Promise.all([query, Artist.count(queryCriteria)])
+        .then((results) => {
+            return {
+                all: results[0],
+                count: results[1],
+                offset: offset,
+                limit: limit
+            }
+        });
+};
+
+const buildQuery = (criteria) => {
+    const query = {};
+    if (criteria.name) {
+        // Create an index for name in MongoDb first via cli
+        // db.artists.createIndex({ name: "text"})
+        query.$text = { $search: criteria.name };
+    }
+    if (criteria.age) {
+        query.age = {
+            $gte: criteria.age.min,
+            $lte: criteria.age.max
+        };
+    }
+
+    if (criteria.yearsActive) {
+        query.yearsActive = {
+            $gte: criteria.yearsActive.min,
+            $lte: criteria.yearsActive.max
+        };
+    }
+    return query;
+}
